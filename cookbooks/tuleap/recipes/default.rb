4faacb39e0ab248812194d3fe5e12f5d51292a9d
Sebastien Nicouleaud <seb@sogilis.com>
2012-07-23 13:16:48 +0200

    Disable CentOS PHP + open port 80

--- cookbooks/tuleap/recipes/default.rb
+++ cookbooks/tuleap/recipes/default.rb
@@ -7,13 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Disable PHP from CentOS repository
+cookbook_file '/etc/yum.repos.d/CentOS-Base.repo' do
+  mode '0644'
+end
+
+# Add Tuleap repository
 cookbook_file '/etc/yum.repos.d/tuleap.repo' do
   mode '0644'
 end
 
+# Install PHP + Tuleap from Tuleap repository
 package 'php-pecl-json'
 package 'tuleap-all'
 
+# Disable SELinux
 cookbook_file '/etc/sysconfig/selinux' do
   mode '0644'
+end
+
+# Open port 80
+cookbook_file '/etc/sysconfig/iptables' do
+  mode '0600'
 end
