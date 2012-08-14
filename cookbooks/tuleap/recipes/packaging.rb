759ec2d22c4172cc7ff33421881c2c2d4da0d809
Sebastien Nicouleaud <seb@sogilis.com>
2012-08-14 13:51:41 +0200

    Fixed production dependencies removal

--- cookbooks/tuleap/recipes/packaging.rb
+++ cookbooks/tuleap/recipes/packaging.rb
@@ -11,14 +11,21 @@ php_base = node['tuleap']['php_base']
 
 # Remove the production dependencies (to prevent conflict with the packages to be built)
 node['tuleap']['production']['dependencies'].each do |name|
-  package name do
+  package eval("\"#{name}\"") do
     action :purge
   end
 end
 
+# Disable official Tuleap repo (to prevent conflict with the packages to be built)
+file '/etc/yum.repos.d/tuleap.repo' do
+  action :delete
+end
+
 # Install the packaging required RPMs
 node['tuleap']['packaging']['dependencies'].each do |name|
-  package eval("\"#{name}\"")
+  package eval("\"#{name}\"") do
+    action :install
+  end
 end
 
 # Add the packaging user to the mock group (required for building RPMs)
