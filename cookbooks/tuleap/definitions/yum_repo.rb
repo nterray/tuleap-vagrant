f5983d009467b3e9a819f1bc8e3e098c9f92a3c6
Sebastien Nicouleaud <seb@sogilis.com>
2012-08-16 14:17:00 +0200

    yum_repo definition + remove development symlink

---
+++ cookbooks/tuleap/definitions/yum_repo.rb
@@ -0,0 +1,29 @@
+define :yum_repo, :action => 'add' do
+  case params[:action].to_s
+  when 'add'
+    if params[:template]
+      template "/etc/yum.repos.d/#{params[:name]}.repo" do
+        source    params[:source]
+        mode      params[:mode]
+        variables params[:variables]
+      end
+    else
+      cookbook_file "/etc/yum.repos.d/#{params[:name]}.repo" do
+        source params[:source]
+        mode   params[:mode]
+      end
+    end
+  when 'remove'
+    `rpm -qa | grep -i #{params[:name]}`.split.each do |name|
+      package name do
+        action :purge
+      end
+    end
+    
+    file "/etc/yum.repos.d/#{params[:name]}.repo" do
+      action :delete
+    end
+  else
+    raise "Invalid yum_repo action: #{params[:action]}"
+  end
+end
