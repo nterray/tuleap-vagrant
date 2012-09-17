bef5836d4b2efa7a136d69f89880f1bdc8e75cbe
Sebastien Nicouleaud <seb@sogilis.com>
2012-09-17 15:39:35 +0200

    Install EPEL/standalone RPMS

--- cookbooks/tuleap/recipes/base.rb
+++ cookbooks/tuleap/recipes/base.rb
@@ -64,3 +64,8 @@ tuleap_yum_repository 'local' do
 end
 
 epel_package 'git'
+epel_package 'perl-HTML-Template'
+
+remote_rpm 'openfire' do
+  source 'http://download.igniterealtime.org/openfire/openfire-3.7.1-1.i386.rpm'
+end
